018dfade501e72d8b5fb5d0020f830525a00a06a
mxthss <matabd36@gmail.com>
2026-04-26 00:44:36 +0200

    Add CORS middleware to server.js for Render deployment

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.static('.'));
